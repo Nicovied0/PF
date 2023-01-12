048c0595721d3a5da824ba7883e8e30c5044f91a
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-01-11 23:40:35 -0300

    change variable

--- client/src/login.js
+++ client/src/login.js
@@ -73,7 +73,7 @@ export function registerUserGoogle(dataUser) {
 
     try {
       const registerData = await axios.post(
-        "http://localhost:3001/api/auth/register",
+        "https://apipf-production-001a.up.railway.app/api/auth/register",
         dataUser
       );
       registerData();

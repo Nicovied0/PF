e930a00ea5760537134784cfd2efd71d53be0aac
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-14 16:06:13 -0300

    kadaosdaskd

--- client/src/const.js
+++ client/src/const.js
@@ -1,5 +1,5 @@
-export const BACK_CONECT = "https://pf-production-bd8a.up.railway.app"
-export const FRONT_URL = 'https://pf-o14h3mvz5-nicovied0.vercel.app'
+export const BACK_CONECT = "https://pf-back-five.vercel.app"
+export const FRONT_URL = 'https://el-campito-refugio.vercel.app'
 
 // https://apipf-production-001a.up.railway.app
 // "https://campito-api-production.up.railway.app" 
